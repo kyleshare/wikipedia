7a26b92555701461c62a2281c592c22f0bfd8617
Kyle <kshare@calpoly.edu>
2021-08-17 10:20:21 -0700

    Create display entry page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    # title of any encyclopedia entry
+    path("search", views.search_entry, name="search_entry"),
+    path("<str:entry>", views.display_entry, name="display_entry"),
 ]
+
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,6 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+import markdown
 
 from . import util
 
@@ -8,3 +10,24 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+#second param is url (string path in urls.py)
+def display_entry(request, entry):
+    entry_info = util.get_entry(entry)
+    entry_html = None
+
+    #convert markdown to html
+    if entry_info:
+        entry_html = markdown.markdown(entry_info)
+    
+
+    return render(request, "encyclopedia/entry.html", {
+        "entry": entry,
+        "entry_info": entry_html
+    })
+
+#Why doesn't this func receive a second param?
+def search_entry(request):
+    print(request)
+    print("Exiting search_entry")
+    return HttpResponse("TEST")
