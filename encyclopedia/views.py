919d68622d84c9ce46e1f5778952fc73e72d7475
Kyle <kshare@calpoly.edu>
2021-08-17 16:34:31 -0700

    Begin search entry function. Display existing pages

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,3 +1,4 @@
+from re import sub
 from django.http.response import HttpResponse
 from django.shortcuts import render
 import markdown
@@ -14,20 +15,63 @@ def index(request):
 #second param is url (string path in urls.py)
 def display_entry(request, entry):
     entry_info = util.get_entry(entry)
-    entry_html = None
+    entry_md = None
 
-    #convert markdown to html
+    #convert info to markdown
     if entry_info:
-        entry_html = markdown.markdown(entry_info)
+        entry_md = markdown.markdown(entry_info)
     
 
     return render(request, "encyclopedia/entry.html", {
         "entry": entry,
-        "entry_info": entry_html
+        "entry_md": entry_md
     })
 
 #Why doesn't this func receive a second param?
 def search_entry(request):
-    print(request)
+    if request.method == "POST":
+        #request.POST includes submitted data & other info such as csrftoken
+        post_data = request.POST
+        entry = post_data['q']
+        entry_info = util.get_entry(entry)
+
+        #get entries where query is a substring
+        if not entry_info:
+            entry_matches = check_substring(entry_info)
+
+            #display search results page of entry_matches
+
+
+            #display error page, no entries found
+            #TODO: Create error page, or return Page Not Found!
+
+        #display existing entry page
+        else:
+            entry_md = markdown.markdown(entry_info)
+
+            return render(request, "encyclopedia/entry.html", {
+                "entry": entry,
+                "entry_md": entry_md
+            })
+        
+
+
+#Check if query is substring of an entry, return entry(s) if so
+def check_substring(substring):
+    entries = util.list_entries()
+    entry_matches = []
+
+    for entry in entries:
+        if substring in entry:
+            entry_matches.append(entry)
+
+    return entry_matches
+            
+
+
+
+
+
+    print(f"test: {test}")
     print("Exiting search_entry")
     return HttpResponse("TEST")
